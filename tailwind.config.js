5eb53deb6015d849c83690eee4f38adb037e3d11
Antoine chauveau <a.chauveau87@gmail.com>
2024-10-24 08:16:25 +0200

    feat : loader et fix error

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,6 +55,9 @@ export default {
           4: "hsl(var(--chart-4))",
           5: "hsl(var(--chart-5))",
         },
+        fontFamily: {
+          lato: ["Lato", "sans-serif"],
+        },
       },
     },
   },
