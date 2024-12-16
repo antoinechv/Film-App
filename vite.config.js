97ea9e773068ee4b6f0b89db24a086d21733f3d5
antoinechauveau <antoine.chauveau@etu.unilim.fr>
2024-12-16 19:19:33 +0100

    WIP

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import path from "path";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-
-
   plugins: [react()],
   base: "/Film-app/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
