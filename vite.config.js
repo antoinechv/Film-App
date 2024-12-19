bc18c044610508010d5b08cc5069c4d11f7b7593
antoinechauveau <antoine.chauveau@etu.unilim.fr>
2024-12-19 23:11:11 +0100

    Fix: Bug app assets

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import path from "path";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/Film-App/",
+  base: "/Film-App/", // add repository name here
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src') // Définit '@' comme alias pour le dossier 'src'
-    }
-  }
 });
